8817360d905dad30dff28504aa1fe64d4e3352cc
Nelson Antonio Cerda Ponce <81193895+NelsonCerdaPonce@users.noreply.github.com>
2021-07-05 21:14:32 -0400

    Delete core directory

--- core/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import Libro
-
-class LibroForm(ModelForm):
-
-    class Meta:
-        modelo= Libro
-        fields = ['isbn', 'nombrelibro', 'autor', 'descripcion', 'categoria']
